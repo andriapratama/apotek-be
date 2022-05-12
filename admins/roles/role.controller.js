73e2f31e698b5d9f715aa9b24a63465240b38811
Andria Pratama <andrilocong1st@gmail.com>
2022-05-12 23:36:19 +0800

    make role controller

---
+++ admins/roles/role.controller.js
@@ -0,0 +1,149 @@
+import { Validator } from "node-input-validator";
+import { v4 as uuidv4 } from "uuid";
+import Role from "./role.js";
+import { responses } from "../../util/response.util.js";
+
+export const createRoleData = async (req, res) => {
+	try {
+		const validator = new Validator(req.body, {
+			name: "required|string",
+		});
+
+		const check = await validator.check();
+		if (!check) {
+			return responses(res, 400, { errors: validator.errors });
+		}
+
+		const foundName = await Role.findOne({
+			where: {
+				name: req.body.name,
+			},
+		});
+
+		if (!foundName || typeof foundName === "string") {
+			const role = await Role.create({
+				role_id: uuidv4(),
+				name: req.body.name.toLowerCase(),
+			});
+
+			return responses(res, 200, "Role was created", { role });
+		} else {
+			return responses(res, 400, "Role name already used");
+		}
+	} catch (error) {
+		console.log(error);
+		return responses(res, 500, "server error");
+	}
+};
+
+export const findAllRoleData = async (req, res) => {
+	try {
+		const limit = 10;
+		const page = parseInt(req.query.page);
+
+		const start = (page - 1) * limit;
+		const end = page * limit;
+
+		const role = await Role.findAndCountAll({
+			order: [["createdAt", "DESC"]],
+			limit: limit,
+			offset: start,
+		});
+
+		const countFilter = role.count;
+		const pagination = {};
+
+		pagination.totalRow = role.count;
+		pagination.totalPage = Math.ceil(countFilter / limit);
+
+		if (end < countFilter) {
+			pagination.next = {
+				page: page + 1,
+				limit,
+			};
+		} else if (start > 0) {
+			pagination.prev = {
+				page: page - 1,
+				limit,
+			};
+		}
+
+		return responses(res, 200, "find all role data", {
+			pagination,
+			role,
+		});
+	} catch (error) {
+		console.log(error);
+		return responses(res, 500, "server error");
+	}
+};
+
+export const findOneRoleData = async (req, res) => {
+	try {
+		const role = await Role.findOne({
+			where: {
+				role_id: req.params.id,
+			},
+		});
+
+		return responses(res, 200, "Find one role data by id", role);
+	} catch (error) {
+		console.log(error);
+		return responses(res, 500, "server error");
+	}
+};
+
+export const updateRoleData = async (req, res) => {
+	try {
+		const validator = new Validator(req.body, {
+			roleId: "required|string",
+			name: "required|string",
+		});
+
+		const check = await validator.check();
+		if (!check) {
+			return responses(res, 400, { errors: validator.errors });
+		}
+
+		const foundName = await Role.findOne({
+			where: {
+				name: req.body.name,
+			},
+		});
+
+		if (!foundName || typeof foundName === "string") {
+			await Role.update(
+				{
+					name: req.body.name,
+				},
+				{
+					where: {
+						role_id: req.body.roleId,
+					},
+				}
+			);
+
+			return responses(res, 200, "Role name was changed");
+		} else {
+			return responses(res, 400, "Role name already used");
+		}
+	} catch (error) {
+		console.log(error);
+		return responses(res, 500, "server error");
+	}
+};
+
+export const deleteRoleData = async (req, res) => {
+	try {
+		await Role.destroy({
+			where: {
+				role_id: req.params.id,
+			},
+		});
+
+		return responses(res, 200, "Role data was deleted");
+	} catch (error) {
+		console.log(error);
+		return responses(res, 500, "server error");
+	}
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 import express from "express";
+import {
+	createRoleData,
+	deleteRoleData,
+	findAllRoleData,
+	findOneRoleData,
+	updateRoleData,
+} from "../admins/roles/role.controller.js";
 
 const router = express.Router();
+
+router.post("/role", createRoleData);
+router.get("/role", findAllRoleData);
+router.get("/role/:id", findOneRoleData);
+router.patch("/role", updateRoleData);
+router.delete("/role/:id", deleteRoleData);
 
 export default router;
